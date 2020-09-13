b201421b328959deaef89b63f85348252984dd08
adamfarooq <adamfarooq@gmail.com>
2020-09-12 21:43:27 -0500

    Adding Template Project

---
+++ 00 Template/Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@F:\Adam\Adam Google Drive\Adam Google Drive\Personal\Documents\Learning\CSinVR\Assignment 2\00 Template\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1550289792216858734.json
